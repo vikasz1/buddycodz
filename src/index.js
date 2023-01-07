f0291175ee030cf8e5ecf80003c45a3bdd67503b
vikasz1 <mauryvikas12345@gmail.com>
2023-01-07 17:01:01 +0530

    frontent bug solved

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import 'bootstrap/dist/css/bootstrap.css'
 import "font-awesome/css/font-awesome.css";
 
 
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
