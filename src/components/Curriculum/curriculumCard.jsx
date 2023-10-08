46763c9e37e66adce91b63e3b788aa0f9e81d8ac
Mehak Yadav <91754656+alkaz1@users.noreply.github.com>
2023-10-08 07:37:40 +0000

    modified courses and enhanced looks

--- src/components/Curriculum/curriculumCard.jsx
+++ src/components/Curriculum/curriculumCard.jsx
@@ -1,26 +1,19 @@
-import React from "react";
-import "./curriculumCard.css";
+// CurriculumCard.js
+import React from 'react';
+import './curriculumCard.css';
+
 const CurriculumCard = (props) => {
   const { image, title, description } = props.data;
 
   return (
-    <div className="my-card single__course__item mb-5">
-      <div className="course__img">
-        <img src={image} alt="" className="w-100" style={{ height: "200px" }} />
+    <div className="my-card">
+      <div className="course-img">
+        <img src={image} alt="" className="w-100" style={{ height: '200px' }} />
       </div>
 
-      <div className="course__details">
-        <h6 className="course__title mb-4 ">{title}</h6>
+      <div className="course-details">
+        <h6 className="course-title">{title}</h6>
         <p>{description}</p>
-        <div className=" d-flex justify-content-between align-items-center"></div>
-
-        <div className=" d-flex ">
-          <p className=" d-flex ">
-            <a href="#register-class" className="btn btn-primary currim`">
-              Book Free Trial
-            </a>
-          </p>
-        </div>
       </div>
     </div>
   );
